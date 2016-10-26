04325156d2d4b5da7c4e6e7294e5e19b8e459bd2
Jhoanes Freitas <jhoanesbarbosa@gmail.com>
2016-10-26 20:31:06 -0300

    algumas configurações

--- jhoanes.com.br/config.php
+++ jhoanes.com.br/config.php
@@ -1,6 +1,9 @@
 <?php
 define('ABSPATH', dirname(__FILE__));
-define('HOME URI', 'http://jhoanes.com.br');
+define('HOME_URI', 'http://jhoanes.com.br');
 define('DEBUG', true);
+define('BD_USER', '');
+define('BD_PASS', '');
+define('BD_NAME', '');
 
 require_once 'load.php';
